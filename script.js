39ae29bb13f1c655cf71d91124f8be66e78c8937
pradojvictor <joaovictorred@hotmail.com>
2024-12-18 17:54:50 -0300

    fix: error line 327, json fix and load localstorage

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const months = [
 ];
 
 
-let eventsArr = [];
+const eventsArr = [];
 getEvents();
 
 function initCalendar() {
@@ -324,7 +324,7 @@ function saveEvents() {
 }
 
 function getEvents() {
-    if (localStorage.getItem("events" === null)) {
+    if (localStorage.getItem("events") === null) {
         return;
     }
     eventsArr.push(...JSON.parse(localStorage.getItem("events")));
